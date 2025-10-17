e938dae3e1d11618b4a04659ec7be408c89eb68b
taz <tazahmedsoft@gmail.com>
2025-10-17 20:50:17 +0600

    find small number

--- day-2/findBigNumber.js
+++ day-2/findBigNumber.js
@@ -1,11 +1,11 @@
 function checkBigNumberInArray(arr) {
   let bigNumber = arr[0];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i + 1]) {
-      bigNumber = arr[i + 1];
+    if (bigNumber < arr[i]) {
+      bigNumber = arr[i];
     }
   }
   return bigNumber;
 }
 
-console.log(checkBigNumberInArray([12, 34, 32, 10, 50, 10, 100]));
+console.log(checkBigNumberInArray([150,12, 34, 32, 10, 50, 10, 100]));
