a9e767784e74ddc76e55913d5c7ddfbb6352de74
taz <tazahmedsoft@gmail.com>
2025-10-19 15:38:22 +0600

    prime number

---
+++ day-4/checkPrimeNumber.js
@@ -0,0 +1,12 @@
+function checkPrimeNumber(number) {
+  let isPrime = true;
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime
+}
+
+
